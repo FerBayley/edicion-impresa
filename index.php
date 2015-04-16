d9f7bfb6a918285f0625809f7a19fd723407e1f4
FerBayley <fernando.bayley@gmail.com>
2015-04-16 01:07:23 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ConvivirPress Edición Impresa</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+
+</head>
+<body>
+		
+		<nav>
+			<section class="Contenedor_menu">
+				<ul>
+					<li>Vea las ediciones anteriores: </li>
+					<li><a href="">2012</a></li>
+					<li><a href="">2013</a></li>
+					<li><a href="">2014</a></li>
+					<li><a href="">2015</a></li>
+					<li> / Volver a la edicion online</li>
+				</ul>
+			</section>
+		</nav> <!-- End of nav -->
+
+		<header>
+			<img src="img/logo.png" alt="Logo convivir Press edicion impresa">
+		</header>
+
+		<!--<section class="Descripcion">
+			<article>
+				<p>
+					Bienvenidos a la descarga en formato digital del Periódico convivir Ahora usted puede leer 
+					la edición del mes en cualquier momento, en todas partes disponible en su casa o en el 
+					trabajo para descargar en formato PDF o leerlo on line. Las presentes son la misma edición 
+					que el periódico impreso en Buenos Aires, Argentina.
+				</p>
+			</article>
+		</section> <!-- End of Descripcion -->
+
+		<section class="Tapa_delMes">
+			<div class="Contenedor_tapa">
+				<div class="col_tapa1">
+					<img src="img/tapa-mes/abril.jpg" alt="">
+				</div> <!-- End of col_tapa1 -->
+
+				<div class="col_tapa2">
+					<article>
+						<p>
+							<q>
+								Bienvenidos a la descarga en formato digital del Periódico convivir Ahora usted 
+								puede leer la edición del mes en cualquier momento, en todas partes disponible 
+								en su casa o en el trabajo para descargar en formato PDF o leerlo on line. Las 
+								presentes son la misma edición que el periódico impreso en Buenos Aires, Argentina.
+							</q>
+						</p>
+					</article>
+
+					<article>
+						<h2>Abril 2015</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque temporibus, maiores 
+							delectus tenetur harum ipsa minima officia voluptates, corrupti placeat aspernatur 
+							similique nihil ex modi quibusdam, incidunt accusantium deleniti nemo?
+						</p>
+						<a href="#">DESCARGAR PDF</a>
+					</article>
+				</div> <!-- End of col_tapa2 -->
+			</div> <!-- End of Contenedor_tapa -->
+		</section> <!-- End of Tapa_delMes -->
+
+		<div class="Titulo_tapa">
+			<h2>PUBLICACIONES 2015</h2>
+		</div> <!-- End of Titulo_tapa -->
+
+		<section class="Tapas">
+			<div class="Tapa1">		
+				<section class="Emboltorio">
+					<img src="img/tapas/enero.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+
+				<section class="Emboltorio">
+					<img src="img/tapas/enero.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+			</div> <!-- end of Tapa1 -->
+
+			<div class="Tapa2">					
+				<section class="Emboltorio">
+					<img src="img/tapas/febrero.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>						
+				</section> <!-- End of Emboltorio -->
+
+				<section class="Emboltorio">
+					<img src="img/tapas/febrero.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>						
+				</section> <!-- End of Emboltorio -->
+			</div> <!-- end of Tapa2 -->
+
+			<div class="Tapa3">
+
+				<section class="Emboltorio">
+					<img src="img/tapas/marzo.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+
+				<section class="Emboltorio">
+					<img src="img/tapas/marzo.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+			</div> <!-- end of Tapa3 -->
+
+			<div class="Tapa4">
+				<section class="Emboltorio">
+					<img src="img/tapas/marzo.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+
+				<section class="Emboltorio">
+					<img src="img/tapas/marzo.jpg" alt="">
+					<h2>Mes de la tapa</h2>
+					<a href="#">DESCARGAR PDF</a>
+				</section> <!-- End of Emboltorio -->
+			</div> <!-- end of Tapa4 -->
+		</section> <!-- End of Tapas -->
+
+
+	<footer>
+		<p>Todos los derechos reservados - &copy;2014 - 
+		<a href="mailto:convivir@convivirpress.com">convivir@convviirpress.com</a></p>
+	</footer>
+	
+</body>
+</html>
